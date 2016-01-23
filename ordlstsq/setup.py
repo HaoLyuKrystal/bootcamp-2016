2584f384d09782335b5f2d91e625b1ab46bddbc2
Hao Lyu <haolyu@berkeley.edu>
2016-01-23 15:19:45 -0800

    Edit setup

---
+++ ordlstsq/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='ordlstsq',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Hao Lyu',
+      author_email='haolyu@berkeley.edu',
+      packages=['ordlstsq'],
+     )
